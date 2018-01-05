f9e0cf31978531a1dcc6193c41be16f85c9661a1
Michael Joyce <ubermichael@gmail.com>
2018-01-05 10:12:44 -0800

    Paginate the lists of clippings under source and category pages.

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -2,31 +2,31 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Clipping;
+use AppBundle\Form\CategoryType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use AppBundle\Entity\Category;
-use AppBundle\Form\CategoryType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Category controller.
  *
  * @Route("/category")
  */
-class CategoryController extends Controller
-{
+class CategoryController extends Controller {
+
     /**
      * Lists all Category entities.
      *
      * @Route("/", name="category_index")
      * @Method("GET")
      * @Template()
-	 * @param Request $request
+     * @param Request $request
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $qb = $em->createQueryBuilder();
         $qb->select('e')->from(Category::class, 'e')->orderBy('e.id', 'ASC');
@@ -38,90 +38,6 @@ class CategoryController extends Controller
             'categories' => $categories,
         );
     }
-    /**
-     * Search for Category entities.
-	 *
-	 * To make this work, add a method like this one to the 
-	 * AppBundle:Category repository. Replace the fieldName with
-	 * something appropriate, and adjust the generated search.html.twig
-	 * template.
-	 * 
-     //    public function searchQuery($q) {
-     //        $qb = $this->createQueryBuilder('e');
-     //        $qb->where("e.fieldName like '%$q%'");
-     //        return $qb->getQuery();
-     //    }
-	 *
-     *
-     * @Route("/search", name="category_search")
-     * @Method("GET")
-     * @Template()
-	 * @param Request $request
-     */
-    public function searchAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-		$repo = $em->getRepository('AppBundle:Category');
-		$q = $request->query->get('q');
-		if($q) {
-	        $query = $repo->searchQuery($q);
-			$paginator = $this->get('knp_paginator');
-			$categories = $paginator->paginate($query, $request->query->getInt('page', 1), 25);
-		} else {
-			$categories = array();
-		}
-
-        return array(
-            'categories' => $categories,
-			'q' => $q,
-        );
-    }
-    /**
-     * Full text search for Category entities.
-	 *
-	 * To make this work, add a method like this one to the 
-	 * AppBundle:Category repository. Replace the fieldName with
-	 * something appropriate, and adjust the generated fulltext.html.twig
-	 * template.
-	 * 
-	//    public function fulltextQuery($q) {
-	//        $qb = $this->createQueryBuilder('e');
-	//        $qb->addSelect("MATCH_AGAINST (e.name, :q 'IN BOOLEAN MODE') as score");
-	//        $qb->add('where', "MATCH_AGAINST (e.name, :q 'IN BOOLEAN MODE') > 0.5");
-	//        $qb->orderBy('score', 'desc');
-	//        $qb->setParameter('q', $q);
-	//        return $qb->getQuery();
-	//    }	 
-	 * 
-	 * Requires a MatchAgainst function be added to doctrine, and appropriate
-	 * fulltext indexes on your Category entity.
-	 *     ORM\Index(name="alias_name_idx",columns="name", flags={"fulltext"})
-	 *
-     *
-     * @Route("/fulltext", name="category_fulltext")
-     * @Method("GET")
-     * @Template()
-	 * @param Request $request
-	 * @return array
-     */
-    public function fulltextAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-		$repo = $em->getRepository('AppBundle:Category');
-		$q = $request->query->get('q');
-		if($q) {
-	        $query = $repo->fulltextQuery($q);
-			$paginator = $this->get('knp_paginator');
-			$categories = $paginator->paginate($query, $request->query->getInt('page', 1), 25);
-		} else {
-			$categories = array();
-		}
-
-        return array(
-            'categories' => $categories,
-			'q' => $q,
-        );
-    }
 
     /**
      * Creates a new Category entity.
@@ -129,11 +45,10 @@ class CategoryController extends Controller
      * @Route("/new", name="category_new")
      * @Method({"GET", "POST"})
      * @Template()
-	 * @param Request $request
+     * @param Request $request
      */
-    public function newAction(Request $request)
-    {
-        if( ! $this->isGranted('ROLE_CONTENT_ADMIN')) {
+    public function newAction(Request $request) {
+        if (!$this->isGranted('ROLE_CONTENT_ADMIN')) {
             $this->addFlash('danger', 'You must login to access this page.');
             return $this->redirect($this->generateUrl('fos_user_security_login'));
         }
@@ -162,13 +77,19 @@ class CategoryController extends Controller
      * @Route("/{id}", name="category_show")
      * @Method("GET")
      * @Template()
-	 * @param Category $category
+     * @param Category $category
+     * @param Request $request
      */
-    public function showAction(Category $category)
-    {
-
+    public function showAction(Request $request, Category $category) {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository(Clipping::class);
+        $query = $repo->categoryQuery($category);
+        $paginator = $this->get('knp_paginator');
+        $clippings = $paginator->paginate($query, $request->query->getint('page', 1), 25);
+        
         return array(
             'category' => $category,
+            'clippings' => $clippings,
         );
     }
 
@@ -178,12 +99,11 @@ class CategoryController extends Controller
      * @Route("/{id}/edit", name="category_edit")
      * @Method({"GET", "POST"})
      * @Template()
-	 * @param Request $request
-	 * @param Category $category
+     * @param Request $request
+     * @param Category $category
      */
-    public function editAction(Request $request, Category $category)
-    {
-        if( ! $this->isGranted('ROLE_CONTENT_ADMIN')) {
+    public function editAction(Request $request, Category $category) {
+        if (!$this->isGranted('ROLE_CONTENT_ADMIN')) {
             $this->addFlash('danger', 'You must login to access this page.');
             return $this->redirect($this->generateUrl('fos_user_security_login'));
         }
@@ -208,12 +128,11 @@ class CategoryController extends Controller
      *
      * @Route("/{id}/delete", name="category_delete")
      * @Method("GET")
-	 * @param Request $request
-	 * @param Category $category
+     * @param Request $request
+     * @param Category $category
      */
-    public function deleteAction(Request $request, Category $category)
-    {
-        if( ! $this->isGranted('ROLE_CONTENT_ADMIN')) {
+    public function deleteAction(Request $request, Category $category) {
+        if (!$this->isGranted('ROLE_CONTENT_ADMIN')) {
             $this->addFlash('danger', 'You must login to access this page.');
             return $this->redirect($this->generateUrl('fos_user_security_login'));
         }
@@ -224,4 +143,5 @@ class CategoryController extends Controller
 
         return $this->redirectToRoute('category_index');
     }
+
 }
