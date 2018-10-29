32d22e3384ddf8cbd4f9ca002702a9a7ecaa2174
Michael Joyce <ubermichael@gmail.com>
2018-10-29 15:05:38 -0700

    do some privacy testing.

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,18 +2,40 @@
 
 namespace AppBundle\Tests\Controller;
 
-use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Nines\UserBundle\DataFixtures\ORM\LoadUser;
+use Nines\UtilBundle\Tests\Util\BaseTestCase;
 
-class DefaultControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-        $client = static::createClient();
+class DefaultControllerTest extends BaseTestCase {
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertStatusCode(200, $client);
-
-        $this->assertContains('Circus', $crawler->text());
+    public function getFixtures() {
+        return [
+            LoadUser::class,
+        ];
     }
+
+    public function testIndex() {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertStatusCode(200, $client);
+        $this->assertContains('Circus', $crawler->text());
+        $this->assertNoCookies($client);
+    }
+
+    public function testLogin() {
+        $client = static::createClient();
+        $formCrawler = $client->request('GET', '/login');
+        $this->assertStatusCode(200, $client);
+        $this->assertCookieCount($client, 1);
+
+        $form = $formCrawler->selectButton('Login')->form([
+            '_username' => LoadUser::USER['username'],
+            '_password' => LoadUser::USER['password'],
+        ]);
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertStatusCode(200, $client);
+        $this->assertCookieCount($client, 1);
+    }
+
 }
