65c164b82de678eda831975a5879b58de12fe061
Joey Takeda <joey.takeda@gmail.com>
2020-06-25 14:07:11 -0700

    Keep options open if stuff is sorted

--- public/js/search.js
+++ public/js/search.js
@@ -10,7 +10,8 @@ jQuery(document).ready(function($){
      * in the advanced search controls, then force it open  */
     var controls = $("#controls");
     var inputs =  controls.find("input:checked, input[type='text'][value]");
-    if (inputs.length > 0){
+    var selectedIndex = $("#clipping_search_order").prop("selectedIndex");
+    if (inputs.length > 0 || selectedIndex > 0){
         controls.attr("aria-expanded", "true");
         controls.addClass("in");
     }
