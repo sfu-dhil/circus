864e82daad6244c60ec60fc577ddf20ea81facba
Michael Joyce <ubermichael@gmail.com>
2020-11-30 09:59:53 -0800

    cleaning up a bit.

--- public/js/editor-config.js
+++ public/js/editor-config.js
@@ -1,46 +1,58 @@
 function getTinyMceConfig(editorUploadPath) {
 
-	return {
-		branding: false,
-		selector: '.tinymce',
-		plugins: 'hr image imagetools link lists paste wordcount code advlist charmap preview searchreplace',
-		relative_urls: false,
-		convert_urls: false,
-		height: 320,
-		menubar: 'edit insert view format tools',
+    return {
+        branding: false,
+        selector: '.tinymce',
+        plugins: 'advlist anchor charmap code help hr image imagetools link ' +
+            'lists paste preview searchreplace table wordcount',
+        relative_urls: false,
+        convert_urls: false,
+        height: 320,
+        menubar: 'edit insert view format table tools help',
 
-		toolbar:
-			"undo redo | styleselect | paste | bold italic | alignleft "
-			+ "aligncenter alignright alignjustify | "
-			+ "bullist numlist outdent indent | link image | code",
+        toolbar: [
+            'undo redo | styleselect | pastetext | bold italic | alignleft aligncenter alignright alignjustify | table',
+            'bullist numlist | outdent indent | link | charmap | code'],
 
-		image_caption: true,
-		images_upload_url: editorUploadPath,
-		images_upload_credentials: true,
-		image_advtab: true,
-		image_title: true,
+        browser_spellcheck: true,
 
-		style_formats_merge: true,
-		style_formats: [
-			{
-				title: 'Image-Center',
-				selector: 'img, figure',
-				styles: {
-					position: 'relative',
-					transform: 'translateX(-50%)',
-					left: '50%'
-				}
-			},
-			{
-				title: 'Image-Left',
-				selector: 'img, figure',
-				classes: 'image-left',
-			},
-			{
-				title: 'Image-Right',
-				selector: 'img, figure',
-				classes: 'image-right',
-			}],
-	};
+        image_caption: true,
+        images_upload_url: editorUploadPath,
+        images_upload_credentials: true,
+        image_advtab: true,
+        image_title: true,
+
+        resize: true,
+        paste_as_text: true,
+        paste_block_drop: true,
+
+        style_formats_merge: true,
+        style_formats: [{
+                title: 'Image Left',
+                selector: 'img, figure',
+                styles: {
+                    'float': 'left',
+                    'margin': '0 10px 0 10px',
+                },
+            },
+            {
+                title: 'Image Center',
+                selector: 'img, figure',
+                styles: {
+                    position: 'relative',
+                    transform: 'translateX(-50%)',
+                    left: '50%',
+                },
+            },
+            {
+                title: 'Image Right',
+                selector: 'img, figure',
+                styles: {
+                    'float': 'right',
+                    'margin': '0 10px 0 10px',
+                },
+            },
+        ],
+    };
 
 }
